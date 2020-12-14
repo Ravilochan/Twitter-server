51f84e6330344ad1ae0e03c2db46c77569ef4bdc
RaviLochan <48204237+Ravilochan@users.noreply.github.com>
2020-12-14 22:13:00 +0530

    Removing DB details

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,5 +1,5 @@
 export default {
   PORT: process.env.PORT || 4000,
   DB_URL:
-    "mongodb+srv://mern:cq3iwBBedA4paAAe@cluster0-npseq.gcp.mongodb.net/twitter?retryWrites=true&w=majority",
+    "",
 };
